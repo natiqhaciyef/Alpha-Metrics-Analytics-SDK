0068338ee1e5e09b99157717eb9384f2786b5767
natighajiyev <natig.s.hajiyev@abb-bank.az>
2026-05-20 10:23:37 +0400

    Activity migration

--- analytics-core/src/main/cpp/analytics_core.cpp
+++ analytics-core/src/main/cpp/analytics_core.cpp
@@ -9,14 +9,14 @@ jobject /* this */,
 jstring eventName,
         jstring payload) {
 
-    // 1. Convert Kotlin Strings to C++ Strings
+    // Convert Kotlin Strings to C++ Strings
     const char* cEvent = env->GetStringUTFChars(eventName, nullptr);
     const char* cPayload = env->GetStringUTFChars(payload, nullptr);
 
-    // 2. Log it
+    // Log it
     __android_log_print(ANDROID_LOG_INFO, "AnalyticsNative", "C++ received event: %s | Payload: %s", cEvent, cPayload);
 
-    // 3. Clean up memory
+    // Clean up memory
     env->ReleaseStringUTFChars(eventName, cEvent);
     env->ReleaseStringUTFChars(payload, cPayload);
 }
--- app/src/main/java/com/natighajiyev/alphametricsanalyticssdk/MainActivity.kt
+++ app/src/main/java/com/natighajiyev/alphametricsanalyticssdk/MainActivity.kt
@@ -1,11 +1,10 @@
-package com.natighajiyev.alpha_metrics_analytics_sdk
+package com.natighajiyev.alphametricsanalyticssdk
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.natighajiyev.alphametricsanalyticssdk.R
 import com.natighajiyev.analytics_core.NativeLogger
 
 class MainActivity : AppCompatActivity() {
